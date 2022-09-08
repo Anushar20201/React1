d0fd07ff2742c05a018a28f783e943a7ad714bf7
Anusha <anushar20201@gmail.com>
2022-09-08 09:29:36 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
+
+function App() {
+
+  return (
+    <div>
+      <Nav />
+      <main>
+          <Gallery></Gallery>
+          <About />        
+      </main>
+    </div>
+  );
+}
+
+export default App;
