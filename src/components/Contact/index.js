e2328af9322ad0f16d02cec3a6b1155da2639978
Anusha <anushar20201@gmail.com>
2022-09-10 17:45:54 -0500

    added comp

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,23 +1,57 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm() {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    // Note that the initial state of the errorMessage is an empty string. Now that we've declared the errorMessage Hook
+    const [errorMessage, setErrorMessage] = useState('');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!errorMessage) {
+            console.log('Submit Form', formState);
+        }
+    }
 
     function handleChange(e) {
-        // setFormState({ ...formState, name: e.target.value }) == this is only for name field; but below is for all fields
-        setFormState({ ...formState, [e.target.name]: e.target.value })
+
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+
+        }
+        else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+        }
+        if (!errorMessage) {
+            // setFormState({ ...formState, name: e.target.value }) == this is only for name field; but below is for all fields
+            setFormState({ ...formState, [e.target.name]: e.target.value })
+            console.log('Handle Form', formState);
+        }
 
     }
+
+
     //In the preceding function, we're using the setFormState function to update the formState value for the name property. We assign the value taken from the input field in the UI with e.target.value and assign this value to the property formState.name. We use the spread operator, ...formState, so we can retain the other key-value pairs in this object. Without the spread operator, the formState object would be overwritten to only contain the name: value key pair.
 
     // Note that console.log(formState) is located outside the handleChange function declaration—because the asynchronous nature of the setFormState function will cause the console.log placed in the function body of handleChange to appear delayed in its ability to sync. However, when we place the console.log in the scope of the ContactForm function, we can see that the state is updated properly, as the following image shows:
 
     return (
         <section>
-            <h1>Contact me</h1>
-            <form id="contact-form">
+            <h1 data-testid="h1tag">Contact me</h1>
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
                     <input type="text" defaultValue={name} onChange={handleChange} name="name" />
@@ -31,10 +65,13 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
-                <button type="submit">Submit</button>
-
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+                <button data-testid="button" type="submit">Submit</button>
             </form>
-
         </section>
     )
 }
