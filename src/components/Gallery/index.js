f191789b7916946e9858a177e35e4702db6f7aaa
Anusha <anushar20201@gmail.com>
2022-09-10 16:02:17 -0500

    added comp

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,27 +1,17 @@
 import React from 'react';
+import PhotoList from '../PhotoList';
 import { capitalizeFirstLetter } from '../../utils/helpers';
-import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery(props) {
+  const { currentCategory } = props;
+  return (
+    <section>
+            {/* capitalizeFirstLetter() helper function to capitalize the name value when it's rendered */}
 
-    const currentCategory = {
-        name: "commercial",
-        description: "Photos of grocery stores, food trucks, and other commercial projects",
-    };
-
-
-    //  capitalizeFirstLetter() helper function to capitalize the name value when it's rendered
-
-    return (
-        <section>
-            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
-            <p>{currentCategory.description}</p>
-            <div>
-                <img src={photo} alt="Commercial Example"
-                    className="img-thumbnail mx-1"></img>
-            </div>
-        </section>
-    );
+      <h1 data-testid="h1tag">{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <PhotoList category={currentCategory.name} />
+    </section>
+  );
 }
-
 export default Gallery;
